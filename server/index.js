ca5a0e9bbce399cd16b13052b60e955d8a83b58f
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-16 16:55:39 +0530

    Added required middlewares

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
 
 const PORT = process.env.PORT || '8000'
 
