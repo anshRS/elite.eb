19f8110196524790ac300b52f683d3622b5c3246
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-17 04:14:45 +0530

    Created line chart component

--- client/src/components/auth/dashboard/Graph.jsx
+++ client/src/components/auth/dashboard/Graph.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { graphData } from "@/data";
 
 const Graph = () => {
-    return (
-        <div className='w-full h-[100%] bg-[#26282B] rounded-lg'>
-            {/* Content for the graph */}
-            Graph
-        </div>
-    )
-}
+  const [chartData, setChartData] = useState(null);
 
-export default Graph
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // api call
+        setChartData({
+          labels: graphData?.map((item) => item.date),
+          datasets: [
+            {
+              label: "Price",
+              data: graphData?.map((item) => item.price),
+              backgroundColor: "#26282B", // Set the background color
+              borderColor: "rgba(75,192,192,1)", // Set the line color
+              borderWidth: 2,
+            },
+          ],
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return chartData ? (
+    <Line
+      className="flex w-full items-center justify-center"
+      data={chartData}
+    />
+  ) : null;
+};
+
+export default Graph;
