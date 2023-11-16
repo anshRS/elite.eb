33f1c5b72c3c6251e31be64d37b8c86c86742d33
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-16 19:46:40 +0530

    Created endpoints for authentication

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import ConnectDB from "./database/db.js";
+import router from "./routes/index.js";
 
 
 dotenv.config();
@@ -12,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/',router);
 
 const PORT = process.env.PORT || '8000'
 const MONGO_URL = process.env.MONGO_URL
