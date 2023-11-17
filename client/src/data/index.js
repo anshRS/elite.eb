eaa6f4d030991a486a4f7e9d3bd81fc08f9b802e
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-17 06:13:29 +0530

    Added dummy data for history

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -6,3 +6,42 @@ export const graphData = [
   { date: "2023-01-05", price: 95 },
   { date: "2023-01-06", price: 102 },
 ];
+
+
+export const history = [
+  {
+    id:1,
+    name:"Apple",
+    date:"2022-01-01",
+    quantity:4,
+    price:100
+  },
+  {
+    id:2,
+    name: "Microsoft",
+    date: "2022-01-10",
+    quantity: 10,
+    price: 150
+  },
+  {
+    id:3,
+    name: "Faceboook",
+    date: "2022-01-04",
+    quantity: 25,
+    price: 104
+  },
+  {
+    id:4,
+    name: "Apple",
+    date: "2022-01-06",
+    quantity: 20,
+    price: 110
+  },
+  {
+    id:5,
+    name: "Netflix",
+    date: "2022-01-03",
+    quantity: 8,
+    price: 110
+  }
+]
